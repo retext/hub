4be3cd148ac0c800ff343dae7ecab4c632cba513
Markus Tacker <m@coderbyheart.de>
2014-10-06 09:08:23 +0200

    Clean up build setup

--- app/cache/test/annotations/27b2ab5451a086a391e3fde45779ba8e181b0ed9#loginAction.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:1:{i:0;O:53:"Retext\\Hub\\ApiBundle\\Controller\\Annotation\\ApiRequest":1:{s:60:"' . "\0" . 'Retext\\Hub\\ApiBundle\\Controller\\Annotation\\ApiRequest' . "\0" . 'model";s:47:"\\Retext\\Hub\\ApiBundle\\\\Request\\LoginLinkRequest";}}');
